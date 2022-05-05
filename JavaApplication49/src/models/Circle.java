b7c20642c5e10600decbe7b6b3ff419d98ed0501
George_Karatzas <karatzas101@hotmail.gr>
2022-05-05 12:19:06 +0300

    added a Circle Sout

---
+++ JavaApplication49/src/models/Circle.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author karat
+ */
+public class Circle {
+    private int x;
+    private int y;
+    private int z;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
+    }
+    
+    
+}
